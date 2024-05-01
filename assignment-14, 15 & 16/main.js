18ed1483cfc007551906e4d953c5c917978bde70
Abdul Rafay Khan <160124493+rktech0078@users.noreply.github.com>
2024-05-01 04:21:20 -0700

    Ex # 17

--- assignment-14, 15 & 16/main.js
+++ assignment-14, 15 & 16/main.js
@@ -8,12 +8,24 @@ var guestList = ['Rafay', 'Naeem', 'Arsalan', 'Farzan', 'Ayan'];
 var notPresentGuest = 'Ayan';
 var newGuest = 'Talha';
 guestList[4] = newGuest;
-for (var i = 0; i < guestList.length; i++) {
-    console.log("Dear Sir/Madam: ".concat(guestList[i], " \n you are invited to the dinner party \n I hope you are comming \n Thanks for getting\n\n "));
-}
-console.log("Mr. ".concat(notPresentGuest, " will not coming to the dinner party"));
+for (var i = 0; i < guestList.length; i++)
+    // {
+    //   console.log(`Dear Sir/Madam: ${guestList[i]} \n you are invited to the dinner party \n I hope you are comming \n Thanks for getting\n\n `);
+    // }
+    console.log("Mr. ".concat(notPresentGuest, " will not coming to the dinner party"));
 // ASSIGNMENT NO # 16
 guestList.unshift('Ali', 'Farzana', 'Ahmed');
-for (var i = 0; i < guestList.length; i++) {
-    console.log("Dear Sir/Madam: ".concat(guestList[i], " \n you are invited to the dinner party, and we decided to add three more members to the tomorrow's dinner party \n I hope you are comming \n Thanks for getting\n\n "));
+// for (let i = 0; i < guestList.length; i++) {
+//     console.log("Dear Sir/Madam: ".concat(guestList[i], " \n you are invited to the dinner party, and we decided to add three more members to the tomorrow's dinner party \n I hope you are comming \n Thanks for getting\n\n "));
+// }
+// ASSIGNMENT NO # 17
+console.log("\n Unfortunately, we does not arrange a big table, so we only invited two members to the dinner party");
+while (guestList.length > 2) {
+    var removeMembers = guestList.pop();
+    console.log("Sorry Dear Sir/Member: ".concat(removeMembers, ", we are sorry but you are not invited to the tommorows dinner part for some reasons"));
 }
+for (var i = 0; i < guestList.length; i++) {
+    console.log("Respected Sir/Madam: ".concat(guestList[i], ", You are still invited to the dinner party"));
+}
+guestList.splice(0, 2);
+console.log(guestList);
--- assignment-14, 15 & 16/main.ts
+++ assignment-14, 15 & 16/main.ts
@@ -10,15 +10,15 @@ let guestList: string[] = ['Rafay','Naeem','Arsalan','Farzan','Ayan'];
 
 // ASSIGNMENT NO # 15
 
-let notPresentGuest = 'Ayan';
-let newGuest = 'Talha'
+// let notPresentGuest = 'Ayan';
+// let newGuest = 'Talha'
 
 guestList[4] = newGuest;
 
 for(let i=0; i<guestList.length; i++)
-{
-  console.log(`Dear Sir/Madam: ${guestList[i]} \n you are invited to the dinner party \n I hope you are comming \n Thanks for getting\n\n `);
-}
+// {
+//   console.log(`Dear Sir/Madam: ${guestList[i]} \n you are invited to the dinner party \n I hope you are comming \n Thanks for getting\n\n `);
+// }
 
 console.log(`Mr. ${notPresentGuest} will not coming to the dinner party`);
 
@@ -26,8 +26,27 @@ console.log(`Mr. ${notPresentGuest} will not coming to the dinner party`);
 
 // ASSIGNMENT NO # 16
 
-guestList.unshift('Ali', 'Farzana', 'Ahmed');
-for (let i = 0; i < guestList.length; i++) {
-    console.log("Dear Sir/Madam: ".concat(guestList[i], " \n you are invited to the dinner party, and we decided to add three more members to the tomorrow's dinner party \n I hope you are comming \n Thanks for getting\n\n "));
-}
+// guestList.unshift('Ali', 'Farzana', 'Ahmed');
+// for (let i = 0; i < guestList.length; i++) {
+//     console.log("Dear Sir/Madam: ".concat(guestList[i], " \n you are invited to the dinner party, and we decided to add three more members to the tomorrow's dinner party \n I hope you are comming \n Thanks for getting\n\n "));
+// }
 
+
+
+
+
+
+
+// ASSIGNMENT NO # 17
+
+console.log("\n Unfortunately, we does not arrange a big table, so we only invited two members to the dinner party");
+
+while(guestList.length>2){
+  let removeMembers = guestList.pop();
+  console.log(`Sorry Dear Sir/Member: ${removeMembers}, we are sorry but you are not invited to the tommorows dinner part for some reasons`);
+}
+for (let i=0; i<guestList.length; i++){
+  console.log(`Respected Sir/Madam: ${guestList[i]}, You are still invited to the dinner party`); 
+}
+guestList.splice(0,2);
+console.log(guestList);
