145a3d84f5e9808e5345bc42edb0acfeafd554dd
Abdul Rafay Khan <160124493+rktech0078@users.noreply.github.com>
2024-05-03 12:12:26 -0700

    updated

--- assignment-18/main.js
+++ assignment-18/main.js
@@ -1,2 +1,22 @@
-var cities = ['Makkah', 'Madina', 'Lahore', 'Islamabad', 'Dubai'];
-console.log("Original Cities: ".concat(cities));
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+// Seeing the World: Think of at least five places in the world you’d like to visit.
+// • Store the locations in a array. Make sure the array is not in alphabetical order.
+var cities = ['Makkah ', 'Madina ', 'Lahore ', 'Islamabad ', 'Dubai \n'];
+// • Print your array in its original order.
+// console.log(`Original Cities: ${cities}`);
+// • Print your array in alphabetical order without modifying the actual list.
+// console.log(`Copy cities: ${[...cities].sort()} \n`);
+// • Show that your array is still in its original order by printing it.
+// console.log(`Original Cities: ${cities} \n`);
+// • Print your array in reverse alphabetical order without changing the order of the original list.
+console.log("Copy cities: ".concat(__spreadArray([], cities, true).sort().reverse(), " \n"));
+// • Show that your array is still in its original order by printing it again.
+console.log("Copy cities: ".concat(__spreadArray([], cities, true).sort().reverse(), " \n"));
--- assignment-18/main.ts
+++ assignment-18/main.ts
@@ -1,2 +1,41 @@
-let cities: string[] = ['Makkah','Madina','Lahore','Islamabad','Dubai']
+// Seeing the World: Think of at least five places in the world you’d like to visit.
+// • Store the locations in a array. Make sure the array is not in alphabetical order.
+let cities: string[] = ['Makkah ','Madina ','Lahore ','Islamabad ','Dubai \n' ]
+
+
+// • Print your array in its original order.
 console.log(`Original Cities: ${cities}`);
+
+
+// • Print your array in alphabetical order without modifying the actual list.
+console.log(`Copy cities: ${[...cities].sort()} \n`);
+
+
+// • Show that your array is still in its original order by printing it.
+console.log(`Original Cities: ${cities} \n`);
+
+
+// • Print your array in reverse alphabetical order without changing the order of the original list.
+console.log(`Copy cities: ${[...cities].sort().reverse()} \n`);
+
+
+// • Show that your array is still in its original order by printing it again.
+console.log(`Original Cities: ${cities} \n`);
+
+
+// • Reverse the order of your list. Print the array to show that its order has changed.
+console.log(`Copy cities: ${cities.sort()} \n`);
+
+
+// • Sort your array so it’s stored in alphabetical order. Print the array to show that its order has been changed.
+console.log(`Copy cities: ${cities.sort().reverse()} \n`);
+
+
+// • Sort to change your array so it’s stored in reverse alphabetical order. Print the list to show that its order has changed.
+console.log(`Copy cities: ${cities.sort()} \n`);
+
+
+
+
+
+
