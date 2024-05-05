dd543f4adeb3347b3d94f9dd3c25f763857b57b0
Abdul Rafay Khan <160124493+rktech0078@users.noreply.github.com>
2024-05-05 07:40:03 -0700

    updated with new exercise

---
+++ assignment-19/main.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var main_1 = require("../assignment-14, 15 , 16 & 17/main");
+console.log("\n printing Guest List given in Ex # 14");
+console.log("\n We had finally invited ".concat(main_1.guestList.length, " from Exercise # 14"));
---
+++ assignment-19/main.ts
@@ -0,0 +1,3 @@
+import{ guestList } from '../assignment-14, 15 , 16 & 17/main'
+console.log(`\n printing Guest List given in Ex # 14`);
+console.log(`\n We had finally invited ${guestList.length} from Exercise # 14`);
